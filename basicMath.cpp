27e480ed7bf41e258953e09d636a424b3c326870
Shem Irekpita <shem.irekpita@stu.ocsb.ca>
2025-09-16 18:28:47 +0000

    commit message

---
+++ basicMath.cpp
@@ -0,0 +1,15 @@
+// Created by: Shem
+// Created on: 9/16/2025
+// This program does basic math.
+// Copyright 2025 Shem
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "7 + 6 =" << 7 + 6 << std::endl;
+    std::cout << "4 - 2 =" << 4 - 2 << std::endl;
+    std::cout << "50/5 =" << 50 / 5 << std::endl;
+    std::cout << "8-4/2 =" << 8 - 4 / 2 << std::endl;
+    std::cout << "4^4 =" << pow(4, 4) << std::endl;
+    std::cout << "1 / 3 =" << float(1) / float(3) << std::endl;
+}
